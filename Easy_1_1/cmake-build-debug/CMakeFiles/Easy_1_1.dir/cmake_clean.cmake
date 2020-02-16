cc69c45ca6b19fa9d8909abcca469bc5e9383525
gelio5 <vlreznik97@gmail.com>
2020-02-16 15:04:44 +0300

    Initial commit for Lab 1_1 and dev branch creating

---
+++ Easy_1_1/cmake-build-debug/CMakeFiles/Easy_1_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Easy_1_1.dir/main.cpp.obj"
+  "Easy_1_1.exe"
+  "Easy_1_1.exe.manifest"
+  "Easy_1_1.lib"
+  "Easy_1_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Easy_1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
